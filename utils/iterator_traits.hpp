6219dc599665f6c66c67ddcea1e988bbc41b062a
Ariane Uhoris <auhoris@mi-r3.msk.21-school.ru>
2021-09-06 20:12:14 +0300

    В процессе написание iterator_traits

---
+++ utils/iterator_traits.hpp
@@ -0,0 +1,86 @@
+#ifndef __UTILS_ITERATOR_TRAITS_HPP__
+# define __UTILS_ITERATOR_TRAITS_HPP__
+
+# include <cstddef>
+# include <map>
+
+namespace ft {
+
+struct input_iterator_tag { };
+struct forward_iterator_tag : public input_iterator_tag { };
+struct bidirectional_iterator_tag : public forward_iterator_tag { };
+struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+
+template <class Category,
+         class T,
+         class Distance = std::ptrdiff_t,
+         class Pointer = T*,
+         class Reference = T&>
+struct iterator {
+    typedef T         value_type;
+    typedef Distance  difference_type;
+    typedef Pointer   pointer;
+    typedef Reference reference;
+    typedef Category  iterator_category;
+};
+
+template<class Iter>
+struct iterator_traits {
+    typedef typename Iter::iterator_category    iterator_category;
+    typedef typename Iter::value_type           value_type;
+    typedef typename Iter::difference_type      difference_type;
+    typedef typename Iter::pointer              pointer;
+    typedef typename Iter::reference            reference;
+};
+
+template<class T>
+struct iterator_traits<T*> {
+    typedef random_access_iterator_tag  iterator_category;
+    typedef T                           value_type;
+    typedef ptrdiff_t                   difference_type;
+    typedef T*                          pointer;
+    typedef T&                          reference;
+};
+
+template<typename Iter>
+class reverse_iterator
+: public iterator<typename iterator_traits<Iter>::iterator_category,
+                typename iterator_traits<Iter>::value_type,
+                typename iterator_traits<Iter>::difference_type,
+                typename iterator_traits<Iter>::pointer,
+                typename iterator_traits<Iter>::reference> {
+ protected:
+     Iter   _iter;
+
+ public:
+     typedef Iter  iterator_type;
+     typedef typename iterator_traits<Iter>::iterator_category  iterator_category;
+     typedef typename iterator_traits<Iter>::value_type  value_type;
+     typedef typename iterator_traits<Iter>::difference_type  difference_type;
+     typedef typename iterator_traits<Iter>::pointer  pointer;
+     typedef typename iterator_traits<Iter>::reference  reference;
+
+     reverse_iterator() : _iter() { }
+     explicit reverse_iterator(iterator_type iter) : _iter(iter) { }
+     reverse_iterator(const reverse_iterator<Iter>& iter) : _iter(iter.base()) { }
+     iterator_type base() const { return (_iter); }
+     reference operator*() const {
+         Iter   tmp = _iter;
+         return (*tmp);
+     }
+     reverse_iterator operator+(difference_type n) const { return (reverse_iterator(_iter - n));}
+     reverse_iterator& operator++() {
+         _iter--;
+         return (*this);
+     }
+     reverse_iterator  operator++(int) {
+         reverse_iterator   tmp(*this);
+         _iter--;
+         return (tmp);
+     }
+
+};
+
+} // namespace ft
+
+#endif /* ifndef __UTILS_ITERATOR_TRAITS_HPP__ */
